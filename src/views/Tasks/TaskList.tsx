773bf506ea40a592500b88c3115e63b45db0dac2
edwin.encinas <Edwin.Encinas@jalasoft.com>
2021-10-05 20:39:35 +0000

    minor change

--- src/views/Tasks/TaskList.tsx
+++ src/views/Tasks/TaskList.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { ItemList, ListParams, Task } from '../../application/types';
+import logo from '../../application/assets/images/logo.svg';
+import './TaskList.scss';
 
 type TaskListProps = {
   data: ItemList<Task> | null;
@@ -11,5 +13,22 @@ export default function TaskList({ data, fetch, loading }: TaskListProps) {
   useEffect(() => {
     fetch({});
   }, [fetch]);
-  return <div>{data}</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
